c1acfe61ffce497588806dcb071e033314de673f
Manu G <manusvathgurudath@gmail.com>
2024-04-16 23:55:48 -0700

    bruh

--- build.js
+++ build.js
@@ -1,13 +1,14 @@
 const convertToWindowsStore = require("electron-windows-store");
 
 convertToWindowsStore({
+  makePri: true,
   identityName: "36696Dysperse.318114851B57C",
   publisher: "CN=05A5E870-040E-4832-AE11-60DF88D43653",
   packageName: "Dysperse",
   packageDisplayName: "Dysperse",
   publisherDisplayName: "Dysperse",
 
-  packageVersion: "1.0.2.0",
+  packageVersion: "1.0.4.0",
   packageBackgroundColor: "transparent",
 
   inputDirectory: "./out/Dysperse-win32-x64",
